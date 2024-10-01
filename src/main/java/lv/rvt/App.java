03223d3b327e8d08ef4fd375957e407385d0a811
23DP1LVidr <166118256+23DP1LVidr@users.noreply.github.com>
2024-10-01 06:32:04 +0000

    Repeating, breaking and remembering (5 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,18 +6,39 @@ public class App {
     {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Ievadiet pēdējo skaitli saskaitīšanai: ");
-        int number = Integer.valueOf(scanner.nextLine());
+        System.out.println("Ievadiet skaitļus (-1, lai pabeigtu ievadi): ");
+        
 
         int sum = 0;
+        int numberOfNumbers = 0;
+        float average = 0;
+        int even = 0;
+        int odd = 0;
 
-        for (int i = 1; i < (number+1); i++) {
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if (number < 0) {
+                if (number > -2) {
+                    break;
+                }
+            }
+            sum = sum + number;
+            numberOfNumbers +=1;
+            average = sum / numberOfNumbers;
 
-           sum+=i;
-
+            if (number % 2 == 0) {
+                even +=1;
+            } else{
+                odd +=1;
+            }
         }
-        System.out.println(sum);
-    }
 
-    
+        System.out.println("Pabeigts");
+        System.out.println("Skaitļu summa ir " + sum + ".");
+        System.out.println("Jūs ievadījāt " + numberOfNumbers + " skaitļus.");
+        System.out.println("Skaitļu vidējā vērtība ir " + average + ".");
+        System.out.println("Jūs ievadījāt " + even + " pāra skaitļus un " + odd + " nepāra skaitļus.");
+
+    }
 } 
