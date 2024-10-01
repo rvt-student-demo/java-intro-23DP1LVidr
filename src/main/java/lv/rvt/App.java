01e5141d343d611c3550ed3c849b62a83e9c1eeb
23DP1LVidr <166118256+23DP1LVidr@users.noreply.github.com>
2024-10-01 10:58:45 +0000

    Divisible by three

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,44 +1,26 @@
 package lv.rvt;
 import java.util.Scanner;
-public class App {
 
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] main) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Ievadiet skaitļus (-1, lai pabeigtu ievadi): ");
-        
+        System.out.println("Ievadiet skaitli: ");
+        int beginning = Integer.valueOf(scanner.nextLine());
+        System.out.println("Ievadiet vēl vienu skaitli: ");
+        int end = Integer.valueOf(scanner.nextLine());
 
-        int sum = 0;
-        int numberOfNumbers = 0;
-        float average = 0;
-        int even = 0;
-        int odd = 0;
+        divisibleByThreeInRange(beginning, end);
+    }
 
-        while (true) {
-            int number = Integer.valueOf(scanner.nextLine());
-            
-            if (number < 0) {
-                if (number > -2) {
-                    break;
-                }
-            }
-            sum = sum + number;
-            numberOfNumbers +=1;
-            average = sum / numberOfNumbers;
-
-            if (number % 2 == 0) {
-                even +=1;
-            } else{
-                odd +=1;
+    public static void divisibleByThreeInRange(int beginning, int end) {
+        for (int i = beginning; i < end; i++) {
+            if (i%3==0) {
+                System.out.println(i);
             }
         }
-
-        System.out.println("Pabeigts");
-        System.out.println("Skaitļu summa ir " + sum + ".");
-        System.out.println("Jūs ievadījāt " + numberOfNumbers + " skaitļus.");
-        System.out.println("Skaitļu vidējā vērtība ir " + average + ".");
-        System.out.println("Jūs ievadījāt " + even + " pāra skaitļus un " + odd + " nepāra skaitļus.");
-
+        if (end%3==0) {
+            System.out.println(end);
+        }
     }
-} 
+}
