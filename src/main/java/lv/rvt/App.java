29c4d8a6e718d4a8f530c72ab4be6a0ecb548fcc
23DP1LVidr <166118256+23DP1LVidr@users.noreply.github.com>
2024-11-12 07:47:03 +0000

    Statistics (4 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,29 +4,40 @@ import java.util.Scanner;
 
 public class App {    
     public static void main(String[] args) {
-        Person pekka = new Person("Pekka");
-        Person antti = new Person("Antti");
-    
-        int i = 0;
-        while (i < 30) {
-            pekka.growOlder();
-            i = i + 1;
+        Statistics statistics = new Statistics();
+        statistics.addNumber(3);
+        statistics.addNumber(5);
+        statistics.addNumber(1);
+        statistics.addNumber(2);
+        System.out.println("Count: " + statistics.getCount());
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Average: " + statistics.average());
+
+
+        //SUM OF USER INPUT
+        Statistics statistika = new Statistics();
+        Statistics statsEven = new Statistics();
+        Statistics statsOdd = new Statistics();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ievadiet skaitļus (-1, lai beigtu):");
+
+        while (true) {
+            int number = scanner.nextInt();
+            
+            if (number == -1) {
+                break;
+            }
+            if (number % 2 == 0) {
+                statsEven.addNumber(number);
+            } else {
+                statsOdd.addNumber(number);
+            }
+            statistika.addNumber(number);
         }
-    
-        antti.growOlder();
-    
-        System.out.println("");
-    
-        if (antti.isOfLegalAge()) {
-            System.out.println(antti.getName() + " is of legal age");
-        } else {
-            System.out.println(antti.getName() + " is underage");
-        }
-    
-        if (pekka.isOfLegalAge()) {
-            System.out.println(pekka.getName() + " is of legal age");
-        } else {
-            System.out.println(pekka.getName() + " is underage ");
-        }
+        System.out.println("Skaitļu summa ir " + statistika.sum());
+        System.out.println("Pāra skaitļu summa ir " + statsEven.sum());
+        System.out.println("Nepāra skaitļu summa ir " + statsOdd.sum());
+
     }
 }
--- src/main/java/lv/rvt/Statistics.java
+++ src/main/java/lv/rvt/Statistics.java
@@ -2,16 +2,29 @@ package lv.rvt;
 
 public class Statistics {
     private int count;
+    private int sum;
 
     public Statistics() {
-        this.count = 0;
+        count = 0;
+        sum = 0;
     }
 
     public void addNumber(int number) {
-        this.count = this.count + 1;
+        count++;
+        sum += number;
     }
 
     public int getCount() {
-        return this.count;
+        return count;
+    }
+    public int sum() {
+        return sum;
+    }
+
+    public double average() {
+        if (count == 0) {
+            return 0;
+        }
+        return (double) sum / count;
     }
 }
